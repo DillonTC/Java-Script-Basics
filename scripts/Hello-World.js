445c6095fc00a8755a2b9c8e29446db2844552b9
--global <--global>
2021-11-02 15:49:51 -0600

    Added Variable File

--- scripts/Hello-World.js
+++ scripts/Hello-World.js
@@ -1,1 +1,2 @@
- 
+ let fullName = "Dillon Campbell";
+ console.log("Hello World! My name is %s", fullName);
---
+++ scripts/variables.js
@@ -0,0 +1,6 @@
+//variables are like named containers for stuff
+//let is a command to declare variables
+//age is the name of the variable
+//16 is the value assigned to age
+let age = 16; //the data type of age is a number
+let fullName = "Dillon Campbell"; //the data type of fullName is string
